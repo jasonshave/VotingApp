589574aaec121db5948e0d1cd56f9b50aa15506e
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-06-02 11:51:14 -0600

    fixing tests#

--- tests/VotingApp.Domain.Tests/UnitTest1.cs
+++ tests/VotingApp.Domain.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+
 namespace VotingApp.Domain.Tests;
 
 public class UnitTest1
@@ -8,8 +10,9 @@ public class UnitTest1
         // arrange
         var host = new Participant("abc", "Frank");
         var mockWorkItemRepo = new Mock<IWorkItemRepository>();
+        var mockLogger = new Mock<ILogger<VotingService>>();
         mockWorkItemRepo.Setup(x => x.Save(It.IsAny<WorkItem>())).Returns((WorkItem y) => y);
-        var subject = new VotingService(mockWorkItemRepo.Object);
+        var subject = new VotingService(mockWorkItemRepo.Object, mockLogger.Object);
 
         // act
         var workItem = subject.CreateWorkItem(host, "Test", true);
