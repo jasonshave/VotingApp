66545736b80cc5e8eea2e46c437a676e3c8ebba7
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-06-02 11:52:16 -0600

    added getworkitem logic (#17)
    * added getworkitem logic
    
    * fixing tests#

--- src/VotingApp.Domain.Abstractions/ICrudRepository.cs
+++ src/VotingApp.Domain.Abstractions/ICrudRepository.cs
@@ -3,7 +3,19 @@
 public interface ICrudRepository<TDomainModel>
     where TDomainModel : IAggregateRoot
 {
+    /// <summary>
+    /// Saves domain model or throws.
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    /// <exception cref="ApplicationException"></exception>
     TDomainModel Save(TDomainModel model);
 
+    /// <summary>
+    /// Returns domain model or an exception.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="NotFoundException"></exception>
     TDomainModel Get(string id);
 }
---
+++ src/VotingApp.Domain.Abstractions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace VotingApp.Domain.Abstractions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+
+    }
+}
