9c178a464f87f5f79e4211863b77137fb2d95a44
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-06-09 12:29:54 -0600

    adding generic test host with logging (#19)

---
+++ tests/VotingApp.TestHost/GenericTestHost.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace VotingApp.TestHost;
+
+public abstract class GenericTestHost
+{
+    protected readonly IHost _host;
+
+    protected GenericTestHost(ITestOutputHelper testOutputHelper, Action<IServiceCollection>? servicesDelegate = null)
+    {
+        _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureLogging(loggingBuilder =>
+            {
+                loggingBuilder.Services.AddSingleton<ILoggerProvider>(new XUnitLoggerProvider(testOutputHelper));
+            })
+            .ConfigureServices(services =>
+            {
+                servicesDelegate?.Invoke(services);
+            })
+            .Build();
+    }
+}
---
+++ tests/VotingApp.TestHost/XUnitLogger.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace VotingApp.TestHost;
+
+internal class XUnitLogger : ILogger
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly string? _categoryName;
+    private readonly LoggerExternalScopeProvider _scopeProvider;
+
+    public static ILogger CreateLogger(ITestOutputHelper testOutputHelper) => new XUnitLogger(testOutputHelper, new LoggerExternalScopeProvider(), "");
+    public static ILogger<T> CreateLogger<T>(ITestOutputHelper testOutputHelper) => new XUnitLogger<T>(testOutputHelper, new LoggerExternalScopeProvider());
+
+    public XUnitLogger(ITestOutputHelper testOutputHelper, LoggerExternalScopeProvider scopeProvider, string? categoryName)
+    {
+        _testOutputHelper = testOutputHelper;
+        _scopeProvider = scopeProvider;
+        _categoryName = categoryName;
+    }
+
+    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
+    public IDisposable BeginScope<TState>(TState state)
+        where TState : notnull => _scopeProvider.Push(state);
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        var sb = new StringBuilder();
+        sb.Append(GetLogLevelString(logLevel))
+          .Append(" [").Append(_categoryName).Append("] ")
+          .Append(formatter(state, exception));
+
+        if (exception is not null)
+        {
+            sb.Append('\n').Append(exception);
+        }
+
+        // Append scopes
+        _scopeProvider.ForEachScope((scope, stringBuilder) =>
+        {
+            stringBuilder.Append("\n => ");
+            stringBuilder.Append(scope);
+        }, sb);
+
+        _testOutputHelper.WriteLine(sb.ToString());
+    }
+
+    private static string GetLogLevelString(LogLevel logLevel)
+    {
+        return logLevel switch
+        {
+            LogLevel.Trace => "trace",
+            LogLevel.Debug => "debug",
+            LogLevel.Information => "info",
+            LogLevel.Warning => "warn",
+            LogLevel.Error => "fail",
+            LogLevel.Critical => "critical",
+            _ => throw new ArgumentOutOfRangeException(nameof(logLevel))
+        };
+    }
+}
+
+internal sealed class XUnitLogger<T> : XUnitLogger, ILogger<T>
+{
+    public XUnitLogger(ITestOutputHelper testOutputHelper, LoggerExternalScopeProvider scopeProvider)
+        : base(testOutputHelper, scopeProvider, typeof(T).FullName)
+    {
+    }
+}
